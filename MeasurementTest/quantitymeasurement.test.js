9810290629174456e5f6cff6ebbd6bdf0e35582c
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-02-15 10:39:17 +0530

    Added : functionality and check inch to feet conversions

--- MeasurementTest/quantitymeasurement.test.js
+++ MeasurementTest/quantitymeasurement.test.js
@@ -2,43 +2,52 @@ const assert = require("chai").assert;
 const converting = require("../Measurement/quantitymeasurement");
 
 describe("comparing inch", function() {
-    //inches
+  //inches
   it("0 inch should return 0 inch", () => {
-    assert.equal(true,converting.inch(0,0));
+    assert.equal(true, converting.inch(0, 0));
   }),
-
     //feet
-  it("0 feet should return 0 feet", () => {
-      assert.equal(true,converting.feet(0,0))
-  }),
-
-   // null check
-  it("when given null should through exception", () => {
-    assert.isNotNull(true,converting.inch(0,0))
-  }),
-
-  //reference check
-  it("checking reference", () => {
-      assert.isDefined(converting.inch(0,0),"Its defined!");
-  }),
-
-  //typeOf
-  it("checking type", () => {
-      assert.typeOf(converting.inch(0,0),'Boolean')
-  }),
-  // null check for feet
-  it("when given null should through exception", () => {
-    assert.isNotNull(true,converting.feet(0,0))
-  }),
-
-  //reference check for feet
-  it("checking reference", () => {
-      assert.isDefined(converting.feet(0,0),"Its defined!");
-  }),
-
-  
-  //typeOf check for feet
-  it("checking type", () => {
-      assert.typeOf(converting.feet(0,0),'Boolean')
-  })
+    it("0 feet should return 0 feet", () => {
+      assert.equal(true, converting.feet(0, 0));
+    }),
+    // null check
+    it("when given null should through exception", () => {
+      assert.isNotNull(true, converting.inch(0, 0));
+    }),
+    //reference check
+    it("checking reference", () => {
+      assert.isDefined(converting.inch(0, 0), "Its defined!");
+    }),
+    //typeOf
+    it("checking type", () => {
+      assert.typeOf(converting.inch(0, 0), "Boolean");
+    }),
+    // null check for feet
+    it("when given null should through exception", () => {
+      assert.isNotNull(true, converting.feet(0, 0));
+    }),
+    //reference check for feet
+    it("checking reference", () => {
+      assert.isDefined(converting.feet(0, 0), "Its defined!");
+    }),
+    //typeOf check for feet
+    it("checking type", () => {
+      assert.typeOf(converting.feet(0, 0), "Boolean");
+    }),
+    //check feet to inch conversion
+    it(" 1 feet equals 12 inch", () => {
+      assert.equal(converting.inchTofeet("feet", "inch", 1), 12);
+    }),
+    //check inch to feet conversion
+    it(" 12 inch equals 1 feet", () => {
+      assert.equal(1, converting.inchTofeet("inch", "feet", 12));
+    }),
+    //check feet not equal to inch
+    it(" 1 feet not equal to 1 inch", () => {
+      assert.notEqual(1, converting.inchTofeet("feet", "inch", 1));
+    }),
+    //check inch not equal to feet
+    it(" 1 inch not equals to 1 feet", () => {
+      assert.notEqual(converting.inchTofeet("inch", "feet", 1));
+    });
 });
